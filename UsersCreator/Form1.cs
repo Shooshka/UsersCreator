9e1131a9bede358c1d4e0cb36df035322ed88641
Vyacheslav Mezetskiy <root@ishoo.ru>
2018-11-26 17:16:00 +0300

    Добавьте файлы проекта.

---
+++ UsersCreator/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsersCreator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int startuser = Convert.ToInt32(numericUpDown2.Value);
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                string currpass = Program.generate();
+                Program.CreateLocalWindowsAccount($"{textBox2.Text + startuser}", currpass, $"{textBox2.Text + startuser}", $"{textBox2.Text + startuser}", true, true);
+                textBox1.AppendText($"{textBox2.Text + startuser} {currpass}{System.Environment.NewLine}");
+                startuser++;
+                
+            }
+        }
+    }
+}
---
+++ UsersCreator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices.AccountManagement;
+
+namespace UsersCreator
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static void CreateLocalWindowsAccount(string username, string password, string displayName, string description, bool canChangePwd, bool pwdExpires)
+        {            
+            {
+                PrincipalContext context = new PrincipalContext(ContextType.Machine);
+                UserPrincipal user = new UserPrincipal(context);
+                user.SetPassword(password);
+                user.DisplayName = displayName;
+                user.Name = username;
+                user.Description = description;
+                user.UserCannotChangePassword = canChangePwd;
+                user.PasswordNeverExpires = pwdExpires;
+                user.Save();                
+
+                GroupPrincipal group = GroupPrincipal.FindByIdentity(context, "Пользователи");
+                group.Members.Add(user);
+                group.Save();
+            }
+        }
+
+        public static string generate()
+        {
+            string high = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string low = "abcdefghijklmnopqrstuvwxyz";
+            string numbers = "1234567890";
+            string password = "";
+
+            Random rnd = new Random();
+            password += high[rnd.Next(0, high.Length)];
+            for (int i = 1; i < 7; i++)
+            {
+                password += low[rnd.Next(0, low.Length)];
+            }
+
+            password += numbers[rnd.Next(0, numbers.Length)];
+
+            return password;
+        }
+    }
+}
